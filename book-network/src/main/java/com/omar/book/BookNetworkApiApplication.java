2db6c8fa3c77cb243ea2d22bc883e323d83ae256
Omar Aji <omar3ajii@gmail.com>
2024-05-02 13:54:02 +0200

    added authentication filter + authenticationProvider

--- book-network/src/main/java/com/omar/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/omar/book/BookNetworkApiApplication.java
@@ -2,12 +2,14 @@ package com.omar.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
 
 }
