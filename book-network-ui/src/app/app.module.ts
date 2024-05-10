fcaadb1fabb7a89ea7ee8c9969aaff45ac65e0cc
Omar Aji <omar3ajii@gmail.com>
2024-05-10 15:56:38 +0200

    angular app created

---
+++ book-network-ui/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [
+    HttpClient
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
